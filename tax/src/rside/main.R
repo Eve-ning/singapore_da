33284e3f853c69132d20b8fff46ce2323fb7b45d
Evening <johnchangqi@hotmail.com>
2019-04-20 23:36:07 +0800

    Update main.R
    Some predecessor code to look at the data

--- tax/src/rside/main.R
+++ tax/src/rside/main.R
@@ -1,1 +1,10 @@
-tax = "C:\\Users\\johnc\\Documents\\Projects\\Data\\singapore\\tax\\"
+require(ggplot2)
+
+tax.path <- "C:\\Users\\johnc\\Documents\\Projects\\Data\\singapore\\tax\\main.csv"
+
+tax <- read.csv(tax.path, header = T, sep = ',', stringsAsFactors = T)
+
+tax.levels = levels(tax$assessed_income_group)
+
+ggplot(tax, aes(year_of_assessment, number_of_taxpayers, colour = assessed_income_group)) + geom_point()
+            
