a6bd67a607c69745e78ee40a62a83f2b9659feba
Evening <johnchangqi@hotmail.com>
2019-04-25 10:39:23 +0800

    Append script to fix bad formatting of dataset

--- enrolment/src/main.R
+++ enrolment/src/main.R
@@ -7,3 +7,30 @@ require(scales)
 require(dplyr)
 require(reshape2)
 
+# Load in data
+enrolment.path <- "C:\\Users\\johnc\\Documents\\Projects\\Data\\singapore\\enrolment\\2017\\main.csv"
+enrolment.raw <- read.csv(enrolment.path, header = T, sep = ',', stringsAsFactors = T)
+
+# Treat the data
+
+# 1. The Sex provided are in MF and F, we should replace MF's with M
+enrolment.MF <- subset(enrolment.raw, sex == "MF") # We extract MF
+enrolment.F <- subset(enrolment.raw, sex == "F")   # We extract F
+
+# Subtract off the numeric columns (4 - 6)
+enrolment.M <- enrolment.MF[,4:6] - enrolment.F[,4:6]
+
+# Column Bind back the non-numeric columns (1 - 3)
+
+# We bind via .F instead of binding to .F 
+# because it automatically fixes the issue of numeric columns
+# appearing before the non-numeric ones
+enrolment.M <- enrolment.F[,1:3] %>% 
+  cbind(enrolment.M) %>% 
+  mutate(sex = "M")
+
+# Join back .M and .F
+enrolment <- enrolment.M %>% 
+  rbind(enrolment.F)
+
+save.image(file = "enrolment.RData")
