eaebb869daac4e10cf938651cda9bb9ffe273326
Evening <johnchangqi@hotmail.com>
2019-05-06 16:10:39 +0800

    Update main plot generating scripts

--- wbt/src/r/main.R
+++ wbt/src/r/main.R
@@ -8,4 +8,24 @@ wbt <- wbt.normalize(wbt)
 source("plotting.R")
 
 wbt.plot.loess.comp(wbt)
-wbt.plot.delta(wbt)
+
+# Generate for Everything
+wbt.plot.delta(wbt,
+               filename = "period.png",
+               smooth = 0.04,
+               scaling = 1.2,
+               smoothing = F)
+
+# Generate for 2015
+wbt.plot.delta(wbt,
+               year.start = 2015,
+               filename = "2015.png",
+               smooth = 0.3,
+               scaling = 8)
+
+# Generate for 2017
+wbt.plot.delta(wbt,
+               year.start = 2017,
+               filename = "2017.png",
+               smooth = 0.5,
+               scaling = 8)
